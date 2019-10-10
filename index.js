1533f7eb0a124d7f6d82d9044ecff57c16f099dd
Donatien <donatiennambinintsoa@gmail.com>
2019-10-10 12:09:20 +0300

    Init overall

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
-const express = require('express')
-const app = express()
+"use strict";
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+const requireStaticAssets = (module, filePath) => {
+  let resolvedFilepath = path.resolve(process.cwd(), filePath)
+  module.exports = readFileSync(resolvedFilepath, "utf8");
+};
+
+require.extensions[".css"] = requireStaticAssets
+require.extensions[".html"] = requireStaticAssets
+
+// Modules declarations
+const express = require("express")
+const ejs = require("ejs");
+const path = require("path");
+const juice = require("juice");
+const cheerio = require("cheerio");
+const mustache = require("mustache");
+const { readFileSync } = require("fs");
+const { minify } = require("html-minifier");
+const { unescape } = require("html-escaper");
+const { celebrate, Joi, errors } = require("celebrate");
+
+const app = express()
+const tachyons = require("tachyons/css/tachyons.css");
+const layout = require("./templates/layout.html");
+
+console.log(tachyons)
+
+const loadRenderer = (templatePath) => {
+  let template = require(templatePath)
+  let withinLayout = mustache.render(layout, {
+    content: template,
+    css: tachyons
+  })
+  let withCSS = mustache.render(withinLayout, {  })
+  let inlinedCSS = juice(withCSS)
+  const $ = cheerio.load(inlinedCSS);
+  $("style").remove()
+  $("*").removeAttr("class");
+  let unescaped = unescape($.html());
+  let ejsFunc = ejs.compile(unescaped)
+  return ejsFunc
+}
+
+const routes = [
+  {
+    path: "/verify-email-ownership",
+    validationRule: { body: {} },
+    renderer: loadRenderer("./templates/verify-email-ownership")
+  }
+]
+
+routes.forEach(route => {
+  app.post(route.path,
+    celebrate(route.validationRule),
+    (req, res, next) => {
+      let rendered = route.renderer(req.body);
+      res.send(rendered)
+      next()
+    }
+  )
 })
+
+app.use(errors())
 
 module.exports = app;
