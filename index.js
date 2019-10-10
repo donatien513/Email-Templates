3df6889431ba5a20eb7d26a443b9f2384f625468
Donatien <donatiennambinintsoa@gmail.com>
2019-10-10 14:34:52 +0300

    update template

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const requireStaticAssets = (module, filePath) => {
   module.exports = readFileSync(filePath, "utf8")
@@ -19,6 +19,8 @@ const { minify } = require("html-minifier")
 const { unescape } = require("html-escaper")
 const { celebrate, Joi, errors } = require("celebrate")
 
+const organization = require("./organization")
+
 const loadRawTemplate = (filename) => {
   let resolvedFilepath = path.resolve(process.cwd(), "templates", filename)
   return readFileSync(resolvedFilepath, "utf8")
@@ -32,10 +34,10 @@ const loadRenderer = (templateFilename) => {
   let template = loadRawTemplate(templateFilename)
   let withinLayout = mustache.render(layout, {
     content: template,
-    css: tachyons
+    css: tachyons,
+    organization
   })
-  let withCSS = mustache.render(withinLayout, {  })
-  let inlinedCSS = juice(withCSS)
+  let inlinedCSS = juice(withinLayout)
   const $ = cheerio.load(inlinedCSS)
   $("style").remove()
   $("*").removeAttr("class")
@@ -59,14 +61,6 @@ const routes = [
 ]
 
 routes.forEach(route => {
-  app.get(route.path,
-    celebrate(route.validationRule),
-    (req, res, next) => {
-      let rendered = route.renderer(req.body)
-      res.send(rendered)
-      next()
-    }
-  )
   app.post(route.path,
     celebrate(route.validationRule),
     (req, res, next) => {
